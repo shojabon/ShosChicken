0263e4f6541f798f7f47aa371b71378c4e75c276
sho <shojabon@gmail.com>
2019-05-06 04:25:38 +0900

    added line function

--- src/main/java/com/shojabon/shochicken/Tools/SParticle/particles/SParticleLine.java
+++ src/main/java/com/shojabon/shochicken/Tools/SParticle/particles/SParticleLine.java
@@ -1,7 +1,7 @@
 package com.shojabon.shochicken.Tools.SParticle.particles;
 
 import com.google.common.collect.Lists;
-import com.shojabon.shochicken.Tools.SParticle.SParticle;
+import com.shojabon.shochicken.Tools.SMagic;
 import com.shojabon.shochicken.Tools.SParticle.interfaces.SParticleForm;
 import net.minecraft.server.v1_12_R1.EnumParticle;
 import org.bukkit.Location;
@@ -11,12 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
-public class SParticleCircle implements SParticleForm {
+public class SParticleLine implements SParticleForm{
 
     private EnumParticle particle;
-    private double radius;
-    private double dots;
+    private Vector end;
+    private double dense;
 
     private Location direction;
     private Vector baseMargin = new Vector();
@@ -27,69 +26,83 @@ public class SParticleCircle implements SParticleForm {
     private boolean reverseStart = false;
 
 
-    public SParticleCircle(EnumParticle particle,double radius, double dots){
-        this.radius = radius;
+    public SParticleLine(EnumParticle particle, Vector end, double dense){
         this.particle = particle;
-        this.dots = dots;
+        this.end = end;
+        this.dense = dense;
     }
 
-    public SParticleCircle setParticle(EnumParticle particle){
+    public SParticleLine setParticle(EnumParticle particle){
         this.particle = particle;
         return this;
     }
-    public SParticleCircle setRadius(double radius){
-        this.radius = radius;
+    public SParticleLine setEnd(Vector end){
+        this.end = end;
         return this;
     }
-    public SParticleCircle setDots(double dots){
-        this.dots = dots;
+    public SParticleLine setDense(double dense){
+        this.dense = dense;
         return this;
     }
 
-    public SParticleCircle setBaseMargin(Vector baseMargin){
+    public SParticleLine setBaseMargin(Vector baseMargin){
         this.baseMargin = baseMargin;
         return this;
     }
-    public SParticleCircle setDistanceAwayMargin(Vector direction, double range){
+    public SParticleLine setDistanceAwayMargin(Vector direction, double range){
         this.distanceAwayMargin = direction.normalize().multiply(range);
         return this;
     }
 
-    public SParticleCircle setDirection(Location direction){
+    public SParticleLine setDirection(Location direction){
         this.direction = direction;
         return this;
     }
-    public SParticleCircle setPerParticleDelay(long delayMills){
+    public SParticleLine setPerParticleDelay(long delayMills){
         this.perParticleDelay = delayMills;
         return this;
     }
-    public SParticleCircle setStartAfter(long startAfterMills){
+    public SParticleLine setStartAfter(long startAfterMills){
         this.startAfter = startAfterMills;
         return this;
     }
 
-    public SParticleCircle setReverse(boolean reverse){
+    public SParticleLine setReverse(boolean reverse){
         this.reverseStart = reverse;
         return this;
+    }
+
+    private List<Vector> line(Vector start, Vector end, double dense){
+        List<Vector> pos = new ArrayList<>();
+        Vector v = new Vector(end.getX() - start.getX(),end.getY() - start.getY(),end.getZ() - start.getZ()).divide(new Vector(start.distance(end), start.distance(end), start.distance(end)));
+        double x = v.getX() * dense;
+        double y = v.getY() * dense;
+        double z = v.getZ() * dense;
+        int d = (int) ((start.distance(end))/dense);
+        for(int i =0; i < d; i++){
+            pos.add(start);
+            start.add(new Vector(x,y,z));
+        }
+        return pos;
     }
 
 
     @Override
     public void playParticle(Location atLocation) {
         Runnable r = () -> {
-            List<Vector> pos = getLocationInfo();
-            if(reverseStart) pos = Lists.reverse(pos);
-            if(startAfter != 0){
-                try{
+            List<Vector> pos = getLocationInfo(atLocation.toVector());
+            if (reverseStart) pos = Lists.reverse(pos);
+            if (startAfter != 0) {
+                try {
                     Thread.sleep(startAfter);
-                }catch (Exception e){
+                } catch (Exception e) {
                 }
             }
-            for(Vector v: pos){
-                if(perParticleDelay != 0){
-                    try{
+            for (Vector v : pos) {
+                if (perParticleDelay != 0) {
+                    try {
                         Thread.sleep(perParticleDelay);
-                    }catch (Exception e){
+                    } catch (Exception e) {
                     }
                 }
                 v = SParticleForm.rotateFunction(v, direction);
@@ -101,26 +114,20 @@ public class SParticleCircle implements SParticleForm {
         }else{
             new Thread(r).start();
         }
+
     }
 
     @Override
     public List<Vector> getLocationInfo() {
-        List<Vector> pos = new ArrayList<>();
-        for(double i =0; i < dots; i++){
-            double x = SParticleForm.sin(Math.toRadians(i*360D/ dots)) * radius;
-            double y = SParticleForm.cos(Math.toRadians(i*360D/ dots)) * radius;
-            Vector l = SParticleForm.rotateFunction(new Vector(x, y, 0), direction);
-            pos.add(l);
-        }
-        return pos;
+        return line(new Vector(0,0,0), end, dense);
     }
 
     @Override
     public List<Vector> getLocationInfo(Vector atVector) {
-        List<Vector> v = getLocationInfo();
-        for(int i =0; i < v.size(); i++){
-            v.set(i, v.get(i).add(atVector));
+        List<Vector> pos = getLocationInfo();
+        for(int i =0; i < pos.size(); i++){
+            pos.set(i, pos.get(i).add(atVector));
         }
-        return v;
+        return null;
     }
 }
